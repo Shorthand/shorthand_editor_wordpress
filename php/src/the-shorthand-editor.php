012bdf1e2db3624dfe1b4d8ad8eb035fa2366d4b
Simon Long <slephant@users.noreply.github.com>
2026-04-23 19:27:56 +1000

    Bump version numbers prior to release

--- php/src/the-shorthand-editor.php
+++ php/src/the-shorthand-editor.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: The Shorthand Editor
  * Plugin URI: https://shorthand.com/products/shorthand-for-wordpress
- * Version: 1.0.3
+ * Version: 1.0.4
  * Description: Build rich, compelling content with Shorthand, the premier story-telling experience.
  * Repository URI: https://github.com/Shorthand/shorthand_editor_wordpress
  * License: GPLv3 or later
@@ -10,7 +10,7 @@
  * Author: Shorthand
  * Author URI: https://shorthand.com/
  * Requires at least: 6.0
- * Tested up to: 6.9.1
+ * Tested up to: 6.9.4
  * Requires PHP: 7.4
  * Text Domain: the-shorthand-editor
  */
